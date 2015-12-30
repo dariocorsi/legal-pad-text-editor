0ddd5dee446f4a680e46473bbfd5ea19945bc9bb
Dario Corsi <dariocorsi@gmail.com>
2015-12-30 11:10:42 -0500

    Changed clear animation.

--- js/index.js
+++ js/index.js
@@ -1,59 +1,58 @@
-var padContent = document.getElementById('pad-content'); 
-var padHeader = document.getElementById('pad-header');
-var padTitle = document.getElementById('pad-title');
-var padBody = document.getElementById('pad-body');
-var padInput = document.getElementById('pad-input');
+var padContent = document.getElementById('pad-content'),
+  padHeader = document.getElementById('pad-header'),
+  padTitle = document.getElementById('pad-title'),
+  padBody = document.getElementById('pad-body'),
+  padInput = document.getElementById('pad-input');
 
-function toggleHeaderShadow(){
+function toggleHeaderShadow() {
   var scrollTop = padInput.scrollTop;
-  if(scrollTop > 0){
+  if (scrollTop > 0) {
     padHeader.classList.add('is-overlapping');
-    padBody.style.backgroundPosition = '0 ' + -scrollTop+'px';
-  } else{
+    padBody.style.backgroundPosition = '0 ' + -scrollTop + 'px';
+  } else {
     padHeader.classList.remove('is-overlapping');
-    padBody.style.backgroundPosition = '0 0';    
+    padBody.style.backgroundPosition = '0 0';
   }
 }
 
-function removePage(){
+function removePage() {
   localStorage.clear();
   padContent.classList.add('removing-page');
-  setTimeout( function(){
+  setTimeout(function () {
     padContent.classList.remove('removing-page');
     padTitle.value = '';
     padInput.value = '';
-  }, 1500)
-
+  }, 1000);
 }
 
-function saveTextAsFile(){
+function saveTextAsFile() {
   var documentContent = padInput.value;
-  var textFileAsBlob = new Blob([documentContent], {type:'text/plain'});
+  var textFileAsBlob = new Blob([documentContent], {
+    type: 'text/plain'
+  });
   var fileNameToSaveAs = padTitle.value ? padTitle.value : 'Untitled Note';
-
   var downloadLink = document.createElement("a");
-  downloadLink.download = fileNameToSaveAs;	
+  downloadLink.download = fileNameToSaveAs;
   downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
   document.getElementsByClassName('pad-binding')[0].appendChild(downloadLink);
   downloadLink.click();
   downloadLink.remove();
 }
 
-function saveToLocalStorage(){
-  var documentTitle = padTitle.value;    
-  var documentContent = padInput.value;  
-  localStorage.setItem('title', documentTitle);  
+function saveToLocalStorage() {
+  var documentTitle = padTitle.value;
+  var documentContent = padInput.value;
+  localStorage.setItem('title', documentTitle);
   localStorage.setItem('content', documentContent);
-  console.log('saved');
 }
 
-function loadFromLocalStorage(){
-  if(localStorage.content){
-    padInput.value = localStorage.content;  
+function loadFromLocalStorage() {
+  if (localStorage.content) {
+    padInput.value = localStorage.content;
   }
-  if(localStorage.title){
-    padTitle.value = localStorage.title;  
-  }  
+  if (localStorage.title) {
+    padTitle.value = localStorage.title;
+  }
 }
 
 padTitle.addEventListener('input', saveToLocalStorage);
